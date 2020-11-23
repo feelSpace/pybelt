b665f83daf7afcfa1cd161ac255e4ac31616b821
David M <meignan.david@gmail.com>
2020-11-23 12:06:02 +0100

    PyPI setup script

---
+++ pybelt/__init__.py
@@ -0,0 +1,7 @@
+
+import logging
+
+
+# Logger
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybelt",
+    version="0.0.1",
+    author="feelSpace GmbH",
+    author_email="dev@feelspace.de",
+    description="An Python library to control the feelSpace naviBelt from your application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/feelSpace/pybelt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+    install_requires=["pyserial", "bleak", "asyncio", "queue", "sys", "threading", "time", "logging", "typing"],
+
+)
