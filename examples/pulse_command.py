a62f27ef24550e99449cf26819d02d78349d27ce
David <meignan.david@gmail.com>
2021-01-04 15:18:48 +0100

    Completes examples for vibration and pulse commands

--- examples/pulse_command.py
+++ examples/pulse_command.py
@@ -4,14 +4,13 @@
 import pybelt
 from examples.connect import interactive_belt_connect, setup_logger
 
-from pybelt.belt_controller import BeltController, BeltConnectionState, BeltControllerDelegate, BeltMode
+from pybelt.belt_controller import BeltController, BeltConnectionState, BeltControllerDelegate, BeltMode, \
+    BeltOrientationType, BeltVibrationTimerOption
 
 
 class Delegate(BeltControllerDelegate):
-
-    def dummy(self):
-        # TODO
-        pass
+    # Belt controller delegate
+    pass
 
 
 def main():
@@ -25,22 +24,67 @@ def main():
         print("Connection failed.")
         return 0
 
-    # 0 -> Stop vibration
-    # 1 -> Three short pulses on the right (90°, channel 0)
-    # 2 -> Continuous long pulses toward West (270°, channel 1)
-    # 3 -> Two times two pulses on front (0°, channel 2)
+    # Change belt mode to APP mode
+    belt_controller.set_belt_mode(BeltMode.APP_MODE)
 
     while belt_controller.get_connection_state() == BeltConnectionState.CONNECTED:
         print("Q to quit.")
+        print("0: Stop vibration.")
+        print("1: Start three short pulses on the right (channel 0).")
+        print("2: Start unlimited long pulses toward West (channel 1).")
+        print("3: Start two series of two pulses on front (channel 2).")
         action = input()
         try:
             action_int = int(action)
-            if action_int == 1:
-                # TODO
-                pass
+            if action_int == 0:
+                belt_controller.stop_vibration()
+            elif action_int == 1:
+                belt_controller.send_pulse_command(
+                    channel_index=0,
+                    orientation_type=BeltOrientationType.ANGLE,
+                    orientation=90,
+                    intensity=None,
+                    on_duration_ms=150,
+                    pulse_period=500,
+                    pulse_iterations=3,
+                    series_period=1500,
+                    series_iterations=1,
+                    timer_option=BeltVibrationTimerOption.RESET_TIMER,
+                    exclusive_channel=False,
+                    clear_other_channels=False
+                )
             elif action_int == 2:
-                # TODO
-                pass
+                belt_controller.send_pulse_command(
+                    channel_index=1,
+                    orientation_type=BeltOrientationType.MAGNETIC_BEARING,
+                    orientation=270,
+                    intensity=None,
+                    on_duration_ms=300,
+                    pulse_period=1000,
+                    pulse_iterations=1,
+                    series_period=1000,
+                    series_iterations=None,
+                    timer_option=BeltVibrationTimerOption.RESET_TIMER,
+                    exclusive_channel=False,
+                    clear_other_channels=False
+                )
+            elif action_int == 3:
+                belt_controller.send_pulse_command(
+                    channel_index=2,
+                    orientation_type=BeltOrientationType.ANGLE,
+                    orientation=0,
+                    intensity=None,
+                    on_duration_ms=150,
+                    pulse_period=250,
+                    pulse_iterations=2,
+                    series_period=1000,
+                    series_iterations=2,
+                    timer_option=BeltVibrationTimerOption.RESET_TIMER,
+                    exclusive_channel=False,
+                    clear_other_channels=False
+                )
+            else:
+                print("Unrecognized input.")
         except ValueError:
             if action.lower() == "q" or action.lower() == "quit":
                 belt_controller.disconnect_belt()
--- examples/vibration_command.py
+++ examples/vibration_command.py
@@ -4,14 +4,13 @@
 import pybelt
 from examples.connect import interactive_belt_connect, setup_logger
 
-from pybelt.belt_controller import BeltController, BeltConnectionState, BeltControllerDelegate, BeltMode
+from pybelt.belt_controller import BeltController, BeltConnectionState, BeltControllerDelegate, BeltMode, \
+    BeltVibrationPattern, BeltOrientationType
 
 
 class Delegate(BeltControllerDelegate):
-
-    def dummy(self):
-        # TODO
-        pass
+    # Belt controller delegate
+    pass
 
 
 def main():
@@ -25,22 +24,39 @@ def main():
         print("Connection failed.")
         return 0
 
-    # 0 -> Stop vibration
-    # 1 -> Vibration on the right (90°, channel 0)
-    # 2 -> Vibration toward West (270°, channel 1)
-    # 3 -> 3 seconds vibration on the left (270°, channel 2)
+    # Change belt mode to APP mode
+    belt_controller.set_belt_mode(BeltMode.APP_MODE)
 
     while belt_controller.get_connection_state() == BeltConnectionState.CONNECTED:
         print("Q to quit.")
+        print("0: Stop vibration.")
+        print("1: Start vibration on the right (channel 0).")
+        print("2: Start vibration toward West (channel 1).")
+        print("3: Start vibration on the left for 3 seconds (channel 2).")
         action = input()
         try:
             action_int = int(action)
-            if action_int == 1:
-                # TODO
-                pass
+            if action_int == 0:
+                belt_controller.stop_vibration()
+            elif action_int == 1:
+                belt_controller.vibrate_at_angle(90, channel_index=0)
             elif action_int == 2:
-                # TODO
-                pass
+                belt_controller.vibrate_at_magnetic_bearing(270, channel_index=1)
+            elif action_int == 3:
+                belt_controller.send_vibration_command(
+                    channel_index=2,
+                    pattern=BeltVibrationPattern.CONTINUOUS,
+                    intensity=None,
+                    orientation_type=BeltOrientationType.ANGLE,
+                    orientation=270,
+                    pattern_iterations=1,
+                    pattern_period=3000,
+                    pattern_start_time=0,
+                    exclusive_channel=False,
+                    clear_other_channels=False
+                )
+            else:
+                print("Unrecognized input.")
         except ValueError:
             if action.lower() == "q" or action.lower() == "quit":
                 belt_controller.disconnect_belt()
