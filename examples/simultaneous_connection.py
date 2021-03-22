b8e2cedf7610ff41a335768b69e7abea7cdab481
David <meignan.david@gmail.com>
2021-03-22 21:17:25 +0100

    Draft simultaneous connection example

---
+++ examples/simultaneous_connection.py
@@ -0,0 +1,9 @@
+"""
+* Note: Before establishing simultaneous connections, you must either 1) deactivate the pairing of the belt, or 2) give a different name to each belt and pair each belt in your OS settings.
+* Scan
+* List available belts
+* If no belt: [S]can again / [Q]uit
+* Else: [C]ontinue / [S]can again / [Q]uit
+* Connect all belts
+* Choices: Start a vibration, stop vibration, quit
+"""
