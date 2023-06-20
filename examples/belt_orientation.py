191f024e6988b158b31f2d6bccfda712e4dc323c
David M <meignan.david@gmail.com>
2023-06-20 14:12:46 +0200

    Adjust belt orientation example

--- examples/belt_orientation.py
+++ examples/belt_orientation.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 # encoding: utf-8
-from threading import Event
+import time
+from datetime import datetime
+from datetime import timedelta
+from threading import Event, Lock
 
 from examples.examples_utility import belt_controller_log_to_stdout, interactive_belt_connection
 from pybelt.belt_controller import BeltController, BeltConnectionState, BeltControllerDelegate
@@ -8,12 +11,27 @@ from pybelt.belt_controller import BeltController, BeltConnectionState, BeltCont
 # Event to stop the script
 button_pressed_event = Event()
 
+# Belt orientation
+orientation_lock = Lock()  # Lock to read and write the heading and period (only necessary for BLE interface)
+belt_heading: int = -1  # Last known belt heading value
+belt_orientation_update_period = timedelta()  # Period between the last two orientation updates
+
 
 class Delegate(BeltControllerDelegate):
 
+    def __init__(self):
+        # __init__ is only used for the orientation notification period
+        self._last_orientation_update_time = datetime.now()  # Time of the last heading update
+
     def on_belt_orientation_notified(self, heading, is_orientation_accurate, extra):
-        print("\rBelt heading {}°, accurate: {}, (y: {}, r: {}, p: {})            ".format(
-            heading, is_orientation_accurate, extra[2], extra[3], extra[4]), end="")
+        global belt_heading
+        global belt_orientation_update_period
+        with orientation_lock:
+            belt_heading = heading
+            # Below code is only for measuring notification period
+            now = datetime.now()
+            belt_orientation_update_period = now - self._last_orientation_update_time
+            self._last_orientation_update_time = now
 
     def on_belt_button_pressed(self, button_id, previous_mode, new_mode):
         button_pressed_event.set()
@@ -30,12 +48,21 @@ def main():
         print("Connection failed.")
         return 0
 
-    # Start orientation notification (should already be started during handshake)
+    # Change orientation notification period
     belt_controller.set_orientation_notifications(True)
 
     print("Press a button on the belt to quit.")
     while belt_controller.get_connection_state() == BeltConnectionState.CONNECTED and not button_pressed_event.is_set():
-        button_pressed_event.wait(timeout=0.2)
+        # Delay for terminal display (not necessary if other processing)
+        time.sleep(0.005)
+        # Retrieve orientation with lock
+        with orientation_lock:
+            heading = belt_heading
+            notification_period = belt_orientation_update_period.total_seconds()
+        # Process orientation
+        print("\rBelt heading: {}°\t (period: {:.3f}s)            ".format(heading, notification_period), end="")
+
+    belt_controller.set_orientation_notifications(False)
     belt_controller.disconnect_belt()
     return 0
 
