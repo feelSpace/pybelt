4c5ec333ed56d391591c30aef3b4cf382ce25d67
David M <meignan.david@gmail.com>
2023-08-28 10:42:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybelt",
-    version="1.2.0",
+    version="1.2.1",
     author="feelSpace GmbH",
     author_email="dev@feelspace.de",
     description="An Python library to control the feelSpace naviBelt from your application",
